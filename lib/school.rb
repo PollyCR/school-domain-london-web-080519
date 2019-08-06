124613793e3f4e8867d1b23c613bfe7e37a13ebe
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-08-06 13:47:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,14 @@
-class School
-  attr_accessor :name
-  
-  def initialize(name)
-    @name = name
-  end 
-end
+#Create a class called School 
+
+#Be able to initialize it with a name 
+
+#Create an empty hash which will act as the roster for the School
+
+#Use grade levels as keys 
+
+#Use names as values
+
+
+#create an add student method with arguments of name and grade 
+
+#add student to grade level key 
